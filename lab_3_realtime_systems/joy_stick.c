29cc57f084d7ba07b523f7eed6976cae8e7f8fd2
Olle Ronstad <olle.ronstad@gmail.com>
2024-02-13 13:31:25 +0100

    added times pressed

--- lab_3_realtime_systems/joy_stick.c
+++ lab_3_realtime_systems/joy_stick.c
@@ -1,9 +1,11 @@
 #include <avr/io.h>
-
 // joystick press is on portB bit 5
 // 0x0001_0000 = 0x40
 #define JOY_PRESS_MASK 0x10
 #define JOY_PRESS_BIT 4
+
+uint8_t times_pressed = 0;
+uint8_t last_joy_state = 0;
 
 void setupJOYSTICK(){
 	// configure the controll register
@@ -12,6 +14,8 @@ void setupJOYSTICK(){
 	// set the pull up resistor PORTB bit 7 one
 	PORTB = PORTB | (1<<JOY_PRESS_BIT);
 	// PORTB = 0xff;
+	last_joy_state = 0;
+	times_pressed = 0;
 }
 
 // returns true if joystick is down false if not
@@ -20,13 +24,16 @@ uint8_t is_joistick_down(){
 }
 
 
-uint8_t last_joy_state = 0;
+
+
 uint8_t joy_release(){
+	uint8_t now = is_joistick_down();
 	// swaps the toggles of s3 and s4 button goes from pressed to not pressed
-	if (is_joistick_down() == 0 && last_joy_state == 1){
-		last_joy_state = is_joistick_down();
+	if (now == 0 && last_joy_state == 1){
+		last_joy_state = now;
+		times_pressed++;
 		return 1;
 	}
-	last_joy_state = is_joistick_down();
+	last_joy_state = now;
 	return 0;
 }
--- lab_3_realtime_systems/joy_stick.h
+++ lab_3_realtime_systems/joy_stick.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <stdint.h>
 
+extern uint8_t times_pressed;
+
 void setupJOYSTICK();
 
 uint8_t is_joistick_down();
